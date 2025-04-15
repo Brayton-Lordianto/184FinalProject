dd5fd22a475062fd6ac421ae2756dd99d01a0e0f
Brayton-Lordianto <braylord30@gmail.com>
2025-04-14 22:29:28 -0700

    Initial Commit

---
+++ 184FinalProject/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  184FinalProject
+//
+//  Created by Brayton Lordianto on 4/14/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
