c8713d4d2e2538eeeb3a4f9c40d150509a30f337
Brayton-Lordianto <braylord30@gmail.com>
2025-04-26 21:21:41 -0700

    skybox view

--- 184FinalProject/Renderer.swift
+++ 184FinalProject/Renderer.swift
@@ -209,11 +209,24 @@ actor Renderer {
         
         let metalAllocator = MTKMeshBufferAllocator(device: device)
         
-        let mdlMesh = MDLMesh.newBox(withDimensions: SIMD3<Float>(4, 4, 4),
+        var mdlMesh = MDLMesh.newBox(withDimensions: SIMD3<Float>(4, 4, 4),
                                      segments: SIMD3<UInt32>(2, 2, 2),
                                      geometryType: MDLGeometryType.triangles,
                                      inwardNormals:false,
                                      allocator: metalAllocator)
+        
+        // MARK: make it a sphere
+        mdlMesh =  MDLMesh.newEllipsoid(
+            withRadii: vector_float3(1, 1, 1), // unit sphere
+            radialSegments: 40,
+            verticalSegments: 40,
+            geometryType: .triangles,
+            inwardNormals: false,
+            hemisphere: false,
+            allocator: metalAllocator
+        )
+        // MARK: ===============
+
         
         let mdlVertexDescriptor = MTKModelIOVertexDescriptorFromMetal(mtlVertexDescriptor)
         
@@ -320,9 +333,11 @@ actor Renderer {
         
         let rotationAxis = SIMD3<Float>(1, 1, 0)
         let modelRotationMatrix = matrix4x4_rotation(radians: rotation, axis: rotationAxis)
-        let modelTranslationMatrix = matrix4x4_translation(0.0, 0.0, -8.0)
+//        let modelTranslationMatrix = matrix4x4_translation(0.0, 0.0, -8.0)
+        let modelTranslationMatrix = matrix4x4_translation(0, 0, 0)
+        let modelScaleMatrix = matrix4x4_scale(-1, 1, 1)
         //        let modelMatrix = modelTranslationMatrix * modelRotationMatrix
-        let modelMatrix = modelTranslationMatrix
+        let modelMatrix = modelTranslationMatrix * modelScaleMatrix
         
         let simdDeviceAnchor = deviceAnchor?.originFromAnchorTransform ?? matrix_identity_float4x4
         
@@ -540,6 +555,13 @@ func matrix4x4_translation(_ translationX: Float, _ translationY: Float, _ trans
                                          vector_float4(0, 1, 0, 0),
                                          vector_float4(0, 0, 1, 0),
                                          vector_float4(translationX, translationY, translationZ, 1)))
+}
+
+func matrix4x4_scale(_ scaleX: Float, _ scaleY: Float, _ scaleZ: Float) -> matrix_float4x4 {
+    return matrix_float4x4.init(columns:(vector_float4(scaleX, 0, 0, 0),
+                                         vector_float4(0, scaleY, 0, 0),
+                                         vector_float4(0, 0, scaleZ, 0),
+                                         vector_float4(0, 0, 0, 1)))
 }
 
 func radians_from_degrees(_ degrees: Float) -> Float {
